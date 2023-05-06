647eab66e1109413e2604bc7a9ff13925b71beea
Reward I Masalla <reward.masalla@trc.co.tz>
2023-05-06 17:01:03 +0300

    end of users backend

--- ProjX/CoreRoot/settings.py
+++ ProjX/CoreRoot/settings.py
@@ -35,8 +35,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    'rest_framework_simplejwt',
     "core",
     "core.user",
+    "core.auth",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "core_user.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS':
+        ['django_filters.rest_framework.DjangoFilterBackend'],
+}
